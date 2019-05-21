bd866c38eb54c27da82d83c481e54d9a0832ccbc
Vachanamruth <49383698+Vachanamruth@users.noreply.github.com>
2019-05-21 09:30:02 +0530

    Create palind.c

---
+++ palind.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int input()
+{
+        int n;
+        printf("read a positive number\n");
+        scanf("%d",&n);
+        return n;
+}
+int compute(int n, int *res,int *temp)
+{
+        int d;
+        *temp=n;
+        while(n!=0)
+        {
+                d=n%10;
+                *res=(*res*10)+d;
+                n=n/10;
+        }
+        if(*temp==n)
+        {
+                return 1;
+        }
+        else
+        {
+                return 0;
+        }
+}
+void output(int temp, int res)
+{
+        printf("the input number is %d\n it's reverse is %d\n",temp,res);
+        return ;
+}
+int main()
+{
+        int n,res=0,temp=0;
+        n=input();
+        if(compute(n,&res,&temp))
+        {
+                output(temp,res);
+                printf("the number is a palindrome\n");
+        }
+        else
+        {
+                output(temp,res);
+                printf("the number is not a palindrome\n");
+        }
+        return 0;
+}

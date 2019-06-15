933aae860d765c48836a39c587cd41647ff21565
Vachanamruth <49383698+Vachanamruth@users.noreply.github.com>
2019-06-15 20:21:34 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int input()
 {
         int n;
@@ -11,12 +12,16 @@ int check(int n)
         int t=1;
         if((n==0)||(n==1))
         {
-                ;
+               t=0;
+        }
+        else if(n<0)
+        {
+                t=0;
         }
         else
         {
                 int i;
-                for(i=2;i<n;i++)
+                for(i=2;i<sqrt(n);i++)
                 {
                         if(n%i==0)
                         {
@@ -39,7 +44,14 @@ int main()
         }
         else
         {
-                printf("%d is not prime",n);
+                if(n>0)
+                {
+                        printf("%d is not prime",n);
+                }
+                else
+                {
+                        printf("invalid input");
+                }
         }
         return 0;
 }
